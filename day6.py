53abc8b0d0a1236b77c37597f6a2ad9a6dc97172
Denis Roussel <droussel@centreon.com>
2022-12-06 20:15:10 -0500

    day 6

---
+++ day6.py
@@ -0,0 +1,13 @@
+with open("day6.txt") as f:
+    data = f.read()
+
+
+def message(data, n=4):
+    for i, _ in enumerate(data):
+        if len(set(data[i : (i + n)])) == n:
+            break
+    return i + n
+
+
+print(message(data, 4))
+print(message(data, 14))
